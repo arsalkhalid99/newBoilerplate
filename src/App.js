09f5cf375a2c080c3ae2659532a0997317dee6fb
Eyyan Amir <ayyanamir6@gmail.com>
2021-10-21 15:37:01 +0500

    packages updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React ,{useEffect} from "react";
 import logo from './logo.svg';
-import { Link } from 'react-router-dom'
 import './App.css';
 import "./scss/style.scss";
-import Main from "./components/Main";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fab,fas,far)
 
 function App() {
+  useEffect(()=>{
+    const gsap = window.gsap;
+    let tl = gsap.timeline();
+    tl.to(".text",1,{
+      x:200,
+  })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -21,7 +33,9 @@ function App() {
         >
           Learn React
         </a>
-      <Main />
+        <FontAwesomeIcon icon={['fas', 'angle-right']} />
+        <FontAwesomeIcon icon={['fab', 'apple']} size="lg" />
+      {/* <Main /> */}
       </header>
     </div>
   );
